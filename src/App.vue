6cda8b3c0849847bd1fe27cbdbc528854e938473
IvanRusso91 <iv.russo91@gmail.com>
2022-05-27 11:20:10 +0200

    Inseriti componenti di base

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <bosy>
+
+    <HeaderComp/>
+
+    <jumbotron-comp/>
+
+    <main>
+
+      <ServicesComp/>
+      <CompanyComp/>
+      <ProjectsComp/>
+      <ResultsComp/>
+      <TouchComp/>
+
+    </main>
+
+    <FooterComp/>
+    
+  </bosy>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import HeaderComp from "./components/HeaderComp.vue";
+import JumbotronComp from "./components/JumbotronComp.vue";
+import ServicesComp from "./components/ServicesComp.vue";
+import CompanyComp from "./components/CompanyComp.vue";
+import ProjectsComp from "./components/ProjectsComp.vue";
+import ResultsComp from "./components/ResultsComp.vue";
+import TouchComp from "./components/TouchComp.vue";
+import FooterComp from "./components/FooterComp.vue";
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComp,
+    JumbotronComp,
+    ServicesComp,
+    CompanyComp,
+    ProjectsComp,
+    ResultsComp,
+    TouchComp,
+    FooterComp,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
